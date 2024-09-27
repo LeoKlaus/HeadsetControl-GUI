6ad4c8272df6c7bc6dd388dfc6b78eaaab5f030a
Nicola <61830443+nicola02nb@users.noreply.github.com>
2024-09-27 09:40:30 +0200

    Updated version

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <QTranslator>
 
 const QString APP_NAME = "HeadsetControl-GUI";
-const QString GUI_VERSION = "0.16.8";
+const QString GUI_VERSION = "0.16.9";
 
 int main(int argc, char *argv[])
 {
