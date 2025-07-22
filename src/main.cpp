c443f9980f86144bc276af0c4016631bc7d83cd4
Nicola <61830443+nicola02nb@users.noreply.github.com>
2025-07-22 11:35:41 +0200

    Bump to 0.19.4

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include <QTranslator>
 
 const QString APP_NAME = "HeadsetControl-GUI";
-const QString GUI_VERSION = "0.19.3";
+const QString GUI_VERSION = "0.19.4";
 
 int main(int argc, char *argv[])
 {
